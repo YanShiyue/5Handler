860e1d4e68aeb518fe85597332d415f37c0e13f3
YanShiyue <3201892594@qq.com>
2019-12-09 22:13:17 +0800

    finish

--- app/src/main/java/com/example/handler/MainActivity.java
+++ app/src/main/java/com/example/handler/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.handler;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int UPDATE_TEXT=1;
+    private TextView text;
+
+    private Handler handler=new Handler(){
+        public void handleMessage(Message message){
+            switch (message.what){
+                case UPDATE_TEXT:
+                    //在这里更新UI操作
+                    text.setText("Hello Handler");
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text=(TextView)findViewById(R.id.text);
+        Button bt=(Button)findViewById(R.id.bt);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Message message=new Message();
+                        message.what=UPDATE_TEXT;
+                        handler.sendMessage(message);//将Messqge对象发送出去
+                    }
+                }).start();
+            }
+        });
     }
 }
